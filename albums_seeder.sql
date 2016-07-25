f5a1b70dfb99e0e8fc9d9c661727ca0c27253042
Craig Rupp <craigrupp@utexas.edu>
2016-07-25 16:38:53 -0500

    Add these quick sql exercises

---
+++ albums_seeder.sql
@@ -0,0 +1,8 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+
+VALUES ('Michael Jackson', 'Bad', 1987, 34, 'Pop'),
+('Nirvana', 'Nevermind', 1991, 30, 'Grunge'),
+('Pink Floyd', 'The Wall', 1979, 30, 'Progessive Rock'),
+('The Beatles', 'Sgt. Peppers Lonely Hearts Club Band', 1967, 32.5, 'Rock');
---
+++ select_exercise.sql
@@ -0,0 +1,9 @@
+USE codeup_test_db;
+SELECT 'Sweet album from Kurt & Gang.' AS 'CAPTION';
+SELECT * FROM albums where genre = 'Grunge';
+
+SELECT 'Melt My FACE.' AS 'CAPTION';
+SELECT * FROM albums where name = 'The Wall';
+
+SELECT 'KING OF DANCE' AS 'CAPTION';
+SELECT * FROM albums where genre = 'Pop';
