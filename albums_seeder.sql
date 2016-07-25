638c696195d84c489ad447223c502cd17c464b90
Craig Rupp <craigrupp@utexas.edu>
2016-07-25 18:34:31 -0500

    Playing with a few more albums SELECT cases

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -2,7 +2,18 @@ USE codeup_test_db;
 
 INSERT INTO albums (artist, name, release_date, sales, genre)
 
-VALUES ('Michael Jackson', 'Bad', 1987, 34, 'Pop'),
+VALUES ('Michael Jackson', 'Bad', 1987, 20.3, 'Pop'),
 ('Nirvana', 'Nevermind', 1991, 30, 'Grunge'),
 ('Pink Floyd', 'The Wall', 1979, 30, 'Progessive Rock'),
-('The Beatles', 'Sgt. Peppers Lonely Hearts Club Band', 1967, 32.5, 'Rock');
+('The Beatles', 'Sgt. Peppers Lonely Hearts Club Band', 1967, 32.5, 'Rock'),
+('Michael Jackson', 'Thriller', 1982, 45.4, 'Pop, rock, R&B'),
+('AC/DC', 'Back in Black', 1982, 25.9, 'Hard rock'),
+('Pink Floyd', 'The Dark Side of the Moon', 1973, 22.7, 'Progressive Rock'),
+('Meat Loaf', 'Bat Out of Hell', 1977, 20.6, 'Hard Rock, Progessive Rock'),
+('Whitney Houston', 'The Bodyguard', 1992, 27.4, 'R&B, Soul, Pop'),
+('Fleetwood Mac', 'Rumours', 1977, 27.9, 'Soft rock'),
+('Led Zeppelin', 'Led Zeppelin IV', 1971, 29.0, 'Hard rock, heavy, metal'),
+('Alanis Morissette', 'Jagged Little Pill', 1995, 24.8, 'Alternative rock'),
+('Celine Dion', 'Falling Into You', 1996, 20.2, 'Pop, Soft rock'),
+('Adele', '21', 2011, 22.3, 'Pop, soul'),
+('Santana', 'Supernatural', 1999, 20.5, 'Rock');
--- select_exercise.sql
+++ select_exercise.sql
@@ -1,9 +1,19 @@
 USE codeup_test_db;
-SELECT 'Sweet album from Kurt & Gang.' AS 'CAPTION';
-SELECT * FROM albums where genre = 'Grunge';
 
-SELECT 'Melt My FACE.' AS 'CAPTION';
-SELECT * FROM albums where name = 'The Wall';
+SELECT 'Pink Floyd Offerings' AS 'CAPTION';
+SELECT * FROM albums where artist = 'Pink Floyd';
 
-SELECT 'KING OF DANCE' AS 'CAPTION';
-SELECT * FROM albums where genre = 'Pop';
+SELECT 'Sgt. Pepper was released ' AS 'CAPTION';
+SELECT release_date FROM albums where name = 'Sgt. Peppers Lonely Hearts Club Band';
+
+SELECT 'Genre for Kirk and Gang' AS 'CAPTION';
+SELECT genre FROM albums where name = 'Nevermind';
+
+SELECT 'Albums released post 1990' AS 'CAPTION';
+SELECT name FROM albums where release_date > 1990;
+
+SELECT 'Album with less than 25 million certified sales' AS 'CAPTION';
+SELECT name FROM albums where sales < 25.0;
+
+SELECT 'Albums with rock considered to be any of its genres' AS 'CAPTION';
+SELECT name FROM albums where genre LIKE '%rock%';
