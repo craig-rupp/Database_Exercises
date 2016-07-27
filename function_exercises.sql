3608a86f1bfcf9e931c3aaa3255409ecd4d8c997
Craig Rupp <craigrupp@utexas.edu>
2016-07-27 18:29:26 -0500

    Function exercise

--- function_exercises.sql
+++ function_exercises.sql
@@ -20,3 +20,33 @@ from employees
 where hire_date between '1990-01-01' and '1999-12-31'
 and birth_date like '%-12-25'
 order by birth_date asc, hire_date desc;
+
+--For Functions Organize first name by gender count and gender
+SELECT COUNT(*), gender 
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+GROUP BY gender
+ORDER by gender asc;
+
+--Combine First and Last Name in Single Column
+SELECT CONCAT(first_name, ' ', last_name), birth_date
+from employees
+where first_name LIKE 'e%'
+and last_name like '%e'
+ORDER BY birth_date desc;
+
+--How many days (DateDIFF) employees have been working born on 12/25 in the 90's
+SELECT emp_no, first_name, last_name, birth_date, DATEDIFF(curdate(), hire_date)
+from employees
+where hire_date between '1990-01-01' and '1999-12-31'
+and birth_date like '%-12-25'
+ORDER by DATEDIFF(curdate(), hire_date) desc;
+
+--Add group by clause 'q' not like 'qu' find count to see employees who share last_name Q but no Qu
+SELECT count(last_name), first_name, last_name, gender
+FROM employees
+where last_name NOT like '%qu'
+AND last_name LIKE 'q%'
+GROUP by first_name, last_name
+ORDER BY count(distinct last_name);
+
