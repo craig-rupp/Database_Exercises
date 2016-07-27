a7e0ce9698785c4fa6eab9a71c4bd53bf2edf7cf
Craig Rupp <craigrupp@utexas.edu>
2016-07-27 09:56:00 -0500

    Add Where Exercise for basic SQL pro queries

---
+++ where_exercises.sql
@@ -0,0 +1,18 @@
+use employees;
+SELECT * FROM employees WHERE hire_date = '1985-01-01';
+
+SELECT emp_no, birth_date, first_name, last_name, gender, hire_date
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+SELECT emp_no, birth_date, first_name, last_name, gender, hire_date
+FROM employees
+WHERE last_name LIKE 'E%';
+
+SELECT emp_no, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+
+SELECT emp_no, last_name, hire_date, birth_date
+FROM employees
+WHERE birth_date LIKE '%-12-25';
