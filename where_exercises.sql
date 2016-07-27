10dc4882e80caa2a8eaadf48015403c600bce6b8
Craig Rupp <craigrupp@utexas.edu>
2016-07-27 12:00:07 -0500

    First Edit for 709 rows, before selecting male

--- where_exercises.sql
+++ where_exercises.sql
@@ -3,7 +3,12 @@ SELECT * FROM employees WHERE hire_date = '1985-01-01';
 
 SELECT emp_no, birth_date, first_name, last_name, gender, hire_date
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+WHERE emp_no >= 10200
+AND (
+	first_name = 'Irena'
+	OR first_name = 'Vidya'
+	OR first_name = 'Maya'
+); 
 
 SELECT emp_no, birth_date, first_name, last_name, gender, hire_date
 FROM employees
@@ -16,3 +21,7 @@ WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 SELECT emp_no, last_name, hire_date, birth_date
 FROM employees
 WHERE birth_date LIKE '%-12-25';
+
+SELECT emp_no, first_name, last_name, gender, hire_date
+FROM employees
+WHERE last_name LIKE '%q%';
