31f5081f948b220c459d30c1b6f931c59fe04534
Craig Rupp <craigrupp@utexas.edu>
2016-07-27 13:39:27 -0500

    First order by answer

---
+++ order_by_exercises.sql
@@ -0,0 +1,9 @@
+use employees;
+
+SELECT emp_no, birth_date, first_name, last_name, gender, hire_date
+FROM employees
+WHERE gender = 'M'
+and (first_name = 'Irena'
+or first_name = 'Vidya'
+or first_name = 'Maya')
+order by first_name asc;
--- where_exercises.sql
+++ where_exercises.sql
@@ -23,7 +23,7 @@ and last_name LIKE '%E';
 SELECT emp_no, last_name, hire_date
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
-AND hire_date LIKE '%-12-25';
+AND birth_date LIKE '%-12-25';
 
 SELECT emp_no, first_name, last_name
 FROM employees
